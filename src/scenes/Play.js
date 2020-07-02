201eb2e4e124d4d98a15fb6292bdee8f98c75665
Aaron Ho <aayho@ucsc.edu>
2020-07-02 14:29:15 -0700

    works again

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,10 +23,11 @@ class Play extends Phaser.Scene {
         var musicConfig = {
             mute: false,
             volume: 1,
+            rate: 1,
             detune: 0,
             seek: 0,
             loop: true,
-            delay: 0,
+            delay: 0
         }
 
         this.music.play(musicConfig);
