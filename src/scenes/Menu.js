6a252f6dde7be1a18d8b91e878e818d7fb65e78b
Aaron Ho <aayho@ucsc.edu>
2020-07-02 21:11:25 -0700

    changed menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,15 +8,19 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+
+
+        this.load.image('realstars', './assets/realstars.png');
+
     }
 
     create() {
         // menu display
         let menuConfig = {
-            fontFamily: 'Courier',
+            fontFamily: 'CheapFire',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            backgroundColor: '#000000',
+            color: '#FF0000',
             align: 'right',
             padding: {
                 top: 5,
@@ -29,11 +33,14 @@ class Menu extends Phaser.Scene {
         let centerX = game.config.width/2;
         let centerY = game.config.height/2;
         let textSpacer = 64;
+        
+        
+        this.realstars = this.add.tileSprite(0, 0, 640, 480, 'realstars').setOrigin(0, 0);
 
         this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(centerX, centerY, 'Use ←→ arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
+        menuConfig.backgroundColor = '#000000';
+        menuConfig.color = '#FF0000';
         this.add.text(centerX, centerY + textSpacer, 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0.5);  
         
         // define keys
